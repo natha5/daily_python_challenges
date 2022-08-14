2cb3379eb590d1e54b9c1a5392a8361c0beb8047
natha5 <ns539@canterbury.ac.uk>
2022-08-14 20:54:11 +0100

    <mod> completed day 1

--- day_1_-_division_and_square_root/day1.py
+++ day_1_-_division_and_square_root/day1.py
@@ -4,13 +4,10 @@ def divide_or_square(input_value):
     result_of_mod = input_value % 5
     if result_of_mod == 0:
         sqrt_input = math.sqrt(input_value)
-
+        print(sqrt_input)
         return sqrt_input
     else:
+        print(result_of_mod)
         return result_of_mod
 
-
-
-
-
-divide_or_square(10)
+divide_or_square(9)
