82c78ce61531c53df412d0371b517ccf9c5b8fdf
natha5 <ns539@canterbury.ac.uk>
2022-08-29 13:03:03 +0100

    <mod> largest_odd printed and returned

--- day_9_-_biggest_odd_number/day9.py
+++ day_9_-_biggest_odd_number/day9.py
@@ -9,5 +9,8 @@ def biggest_odd(str_input):
         if i_to_int % 2 == 1:
             if i_to_int > largest_odd:
                 largest_odd = i_to_int
+    print(largest_odd)
+    return largest_odd
+
 
 biggest_odd('23569')
