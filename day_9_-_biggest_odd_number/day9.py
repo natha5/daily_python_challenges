d18f002837a2f65b5ec3faa3a293c05a32cd9943
natha5 <ns539@canterbury.ac.uk>
2022-08-29 13:02:10 +0100

    <add> biggest_odd()

---
+++ day_9_-_biggest_odd_number/day9.py
@@ -0,0 +1,13 @@
+
+def biggest_odd(str_input):
+    char_list = list(str_input)
+
+    largest_odd = 0
+
+    for i in char_list:
+        i_to_int = int(i)
+        if i_to_int % 2 == 1:
+            if i_to_int > largest_odd:
+                largest_odd = i_to_int
+
+biggest_odd('23569')
