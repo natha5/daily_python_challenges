aa66bb77958bef50ff73c7f747c1b2cfc8a852ba
natha5 <ns539@canterbury.ac.uk>
2022-08-31 11:00:36 +0100

    <add> equal_strings()

---
+++ day_11_-_are_they_equal/day11.py
@@ -0,0 +1,23 @@
+
+def equal_strings(string_one, string_two):
+    len_one = len(string_one)
+    len_two = len(string_two)
+
+    are_equal = False
+
+    if len_one == len_two:
+        are_equal = True
+    else:
+        print(are_equal)
+        return are_equal
+
+    for i in string_one:
+        string_two.find(i)
+        if i == -1:
+            are_equal = False
+            print(are_equal)
+            return are_equal
+        else:
+            are_equal = True
+
+
