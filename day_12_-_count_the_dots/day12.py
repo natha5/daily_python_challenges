8962e96b306f63a1b6c3fa2dc929b579be92f681
natha5 <ns539@canterbury.ac.uk>
2022-09-01 20:05:49 +0100

    <mod> fixed increment

--- day_12_-_count_the_dots/day12.py
+++ day_12_-_count_the_dots/day12.py
@@ -1,12 +1,12 @@
-
 def count_dots(input_string):
-    string_as_list =  list(input_string)
+    string_as_list = list(input_string)
     count_of_dots = 0
     for i in string_as_list:
         if i == '.':
-            count_of_dots = +1
+            count_of_dots += 1
 
     print(count_of_dots)
-    return(count_of_dots)
+    return count_of_dots
+
 
 count_dots('h.e.l.p.')
