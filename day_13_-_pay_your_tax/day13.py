1532228d008b6064d5d3a4db232392c6b5c14f69
natha5 <ns539@canterbury.ac.uk>
2022-09-12 16:55:52 +0100

    <mod> fixed percentage calculation

--- day_13_-_pay_your_tax/day13.py
+++ day_13_-_pay_your_tax/day13.py
@@ -2,15 +2,18 @@
 def your_vat():
     vat_percentage = 101
     while vat_percentage > 100:
-        intial_price = input("Enter item price: ")
-        vat_percentage = input("Enter item VAT (%): ")
+        initial_price = int(input("Enter item price: "))
+        vat_percentage = int(input("Enter item VAT (%): "))
 
-        if(vat_percentage > 100):
+        if vat_percentage > 100:
             print("VAT must be 100% or lower ")
 
-    vat_amount = (intial_price / vat_percentage) * 100
+    vat_amount = (vat_percentage * initial_price) / 100
 
-    total_price = vat_amount + intial_price
+    total_price = vat_amount + initial_price
 
     print("VAT inclsive price = ", total_price)
+
+
+your_vat()
 
