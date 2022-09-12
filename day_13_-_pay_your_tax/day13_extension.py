e1446d285a94a9435f9edb0496a878f7caa2ded3
natha5 <ns539@canterbury.ac.uk>
2022-09-12 17:06:36 +0100

    <mod> snake emoji now printed

--- day_13_-_pay_your_tax/day13_extension.py
+++ day_13_-_pay_your_tax/day13_extension.py
@@ -1,8 +1,10 @@
+import emoji
+
 
 def Python_snakes(input_value):
     for i in range(input_value):
         for j in range(i):
-            print('i', end = ' ')
+            print(emoji.emojize(':snake:'), end = ' ')
         print(' ')
 
 Python_snakes(7)
