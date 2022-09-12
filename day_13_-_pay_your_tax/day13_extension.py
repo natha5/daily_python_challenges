f40cb8053a3641dfa93bccd9f548a03c79b0e6ad
natha5 <ns539@canterbury.ac.uk>
2022-09-12 17:02:52 +0100

    <mod> pyramid now prints correctly

--- day_13_-_pay_your_tax/day13_extension.py
+++ day_13_-_pay_your_tax/day13_extension.py
@@ -1,8 +1,8 @@
 
 def Python_snakes(input_value):
     for i in range(input_value):
-        for j in range(input_value):
-            print('i ')
-
+        for j in range(i):
+            print('i', end = ' ')
+        print(' ')
 
 Python_snakes(7)
